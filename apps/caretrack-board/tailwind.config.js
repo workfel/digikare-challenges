5c625da6a99519444f16e3e04065744e27a6bd40
workfel <workfel@gmail.com>
2023-07-24 11:01:52 +0200

    feat(config): Setup tailwind

---
+++ apps/caretrack-board/tailwind.config.js
@@ -0,0 +1,27 @@
+const {createGlobPatternsForDependencies} = require('@nx/angular/tailwind');
+const {join} = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    colors: {
+      primary: {
+        light: '#5eead4',
+        DEFAULT: '#14b8a6',
+        dark: '#0f766e',
+      },
+      secondary: {
+        light: '#bae6fd',
+        DEFAULT: '#0ea5e9',
+        dark: '#0369a1',
+      },
+      white: '#ffffff',
+      black: '#000000',
+    },
+  },
+  plugins: [],
+};
