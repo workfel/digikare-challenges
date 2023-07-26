de7801d9217772ea925693d0101dc68cc9259c27
workfel <workfel@gmail.com>
2023-07-26 18:27:42 +0200

    feat(config): Configure i18n + routing

--- apps/caretrack-board/src/app/app.config.ts
+++ apps/caretrack-board/src/app/app.config.ts
@@ -1,10 +1,32 @@
-import { ApplicationConfig } from '@angular/core';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { appRoutes } from './app.routes';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter, withEnabledBlockingInitialNavigation,} from '@angular/router';
+import {appRoutes} from './app.routes';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {CoreModule} from "./core/core.module";
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(
+      HttpClientModule,
+      TranslateModule.forRoot({
+        defaultLanguage : 'fr',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        }
+      }),
+      //
+      CoreModule
+    ),
+
+  ],
 };
--- apps/caretrack-board/src/app/app.routes.ts
+++ apps/caretrack-board/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Route } from '@angular/router';
+import {Route} from '@angular/router';
+import {CreateCaretrackComponent} from "./pages/create-caretrack/create-caretrack.component";
+import {CaretracksComponent} from "./pages/caretracks/caretracks.component";
+import {CaretrackComponent} from "./pages/caretrack/caretrack.component";
+import {NavigationRoutes} from "@digikare-challenges/core-domain";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {path: NavigationRoutes.HOME, component: CaretracksComponent},
+  {path: NavigationRoutes.CREATE, component: CreateCaretrackComponent},
+  {path: 'caretracks/:id', component: CaretrackComponent}
+];
